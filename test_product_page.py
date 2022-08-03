1a2f028a61b129f1334b5fc689ca3c7a503ded1f
VmTalik <vmarlyan@mail.ru>
2022-08-03 14:12:39 +0300

    test product page link changed

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,8 @@
 from .pages.product_page import ProductPage
 
+
 def test_guest_can_add_product_to_basket(browser):
-    link ="http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
@@ -9,4 +10,3 @@ def test_guest_can_add_product_to_basket(browser):
     page.should_be_add_to_basket_button()
     page.good_added_to_basket()
     page.basket_value()
-
